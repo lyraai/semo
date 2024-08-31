45843e76e46691bb71d470404306222be473ecc3
bailangcheng <chenglang00@gmail.com>
2024-08-31 14:12:40 +0900

    Updates

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
 // my-semo-app\App.tsx
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './store'; // 正确导入store
+import { store } from './store';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './features/counter/counterSlice';
-import { RootState } from './store'; 
+import { RootState } from './store';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-function Main() {  // 将Main函数直接放在App.tsx中
+function Main() {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
 
   return (
     <View style={styles.container}>
+      {/* 确保所有文本内容在<Text>组件内 */}
       <Text>Count: {count}</Text>
       <Button title="Increment" onPress={() => dispatch(increment())} />
       <Button title="Decrement" onPress={() => dispatch(decrement())} />
@@ -25,7 +26,7 @@ function Main() {  // 将Main函数直接放在App.tsx中
 export default function App() {
   return (
     <Provider store={store}>
-      <Main />  {/* 渲染Main组件 */}
+      <Main />
     </Provider>
   );
 }
